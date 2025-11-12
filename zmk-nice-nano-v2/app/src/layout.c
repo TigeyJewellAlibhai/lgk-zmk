02552b75bc57ddc1e191ad303ad6685d71978f79
Tigris <tigeysky@gmail.com>
2025-11-11 19:33:40 -0500

    Add ZMK configuration

---
+++ zmk-nice-nano-v2/app/src/layout.c
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2025
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <zephyr/kernel.h>
+
+// This file can be used for custom layout processing if needed
+// For ZMK, the keymap is primarily defined in the keymap.keymap file
