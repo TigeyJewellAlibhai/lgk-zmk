c0534bd9516ab5e0f6db76939a837c27c6e7a25b
Tigris <tigeysky@gmail.com>
2025-11-11 21:36:36 -0500

    Remove conflicting app folder - keep only shield config

--- zmk-nice-nano-v2/app/src/layout.c
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2025
- * SPDX-License-Identifier: MIT
- */
-
-#include <zephyr/kernel.h>
-
-// This file can be used for custom layout processing if needed
-// For ZMK, the keymap is primarily defined in the keymap.keymap file
